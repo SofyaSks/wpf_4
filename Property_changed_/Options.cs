402c940139738bb1ddadfa80c1e836b0c84235b9
sks <ssurovneva@mail.ru>
2023-05-14 16:26:37 +0300

    Add project files.

---
+++ Property_changed_/Options.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property_changed_
+{
+    class Options : INotifyPropertyChanged
+    {
+        private int musicVolume;
+        public int MusicVolume
+        {
+            get { return musicVolume; }
+            set
+            {
+                musicVolume = value;
+                //if (PropertyChanged != null)
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MusicVolume)));
+            }
+        }
+
+        private int soundVolume;
+        public int SoundVolume
+        {
+            get => soundVolume;
+            set
+            {
+                soundVolume = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SoundVolume)));
+            }
+        }
+
+        private int fullScreenValue;
+        public int IsFullScreen {
+            get => fullScreenValue;
+            set
+            {
+                soundVolume = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(fullScreenValue)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ menu_/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace menu_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileNew_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Стерeть ли документ?", "Подтвердите", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                textb.Clear();
+        }
+    }
+}
